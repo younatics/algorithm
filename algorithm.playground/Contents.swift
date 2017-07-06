ef3dd0c62cd656973d5593e97601a1af3728f455
Seungyoun Yi <younatics@gmail.com>
2017-07-06 17:14:29 +0900

    add more codes

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,14 +1,17 @@
-//: Playground - noun: a place where people can play
+//: Playground - noun: a place where younatics can play
 
 import UIKit
 
 var array = [0,1,2,3,4,5,6,7,8,9,10]
 
-// Return even number
+// Filter
 let evenArray = array.filter( { (value: Int) -> Bool in return (value % 2 == 0) } )
 print(evenArray)
 
-// Multiple array
+// Map
 let multipliedArray = array.map { $0 * 2 }
 print(multipliedArray)
 
+// Reduce
+let reduce = array.reduce(0) { $0 + $1 }
+print(reduce)
