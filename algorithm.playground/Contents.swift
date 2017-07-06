2977fb4676965e70df139a6061426cd119f565ee
Seungyoun Yi <younatics@gmail.com>
2017-07-06 17:08:10 +0900

    add more algorithm

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -3,5 +3,12 @@
 import UIKit
 
 var array = [0,1,2,3,4,5,6,7,8,9,10]
-let oddArray = array.filter( { (value: Int) -> Bool in return (value % 2 == 0) } )
-print(oddArray)
+
+// Return even number
+let evenArray = array.filter( { (value: Int) -> Bool in return (value % 2 == 0) } )
+print(evenArray)
+
+// Multiple array
+let multipliedArray = array.map { $0 * 2 }
+print(multipliedArray)
+
